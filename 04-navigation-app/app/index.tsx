344b838bbec645f1c946d0dff314f896da6f9478
Victor Vega <vegav2605@gmail.com>
2025-10-29 17:19:07 -0600

    Add nativewind/tailwind

---
+++ 04-navigation-app/app/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { Text, View } from "react-native";
+import "../global.css";
+
+const index = () => {
+  return (
+    <View className="mx-2 py-10">
+      <Text className="text-3xl font-bold text-blue-500">Hola Mundo</Text>
+    </View>
+  );
+};
+
+export default index;
